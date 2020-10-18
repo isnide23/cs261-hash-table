1cd75f931fe993a2b742329cd2a69fa0778687d3
Ian Snyder <isnide23@gmail.com>
2020-10-17 21:10:39 -0700

    __setite__ & __getitem___ tests finished

--- hash_table.py
+++ hash_table.py
@@ -3,7 +3,44 @@
 # Your implementation should pass the tests in test_hash_table.py.
 # YOUR NAME
 
-
 class HashTable:
 
-    pass
+    def __init__(self, size = 10):
+        self.size = size
+        self.data = []
+
+        i = 0
+        while i < self.size:
+            self.data.append([])
+            i += 1
+
+    def __getitem__(self, key):
+        index = hash(key) % 10
+        return self.data[index]
+        
+    def __setitem__(self, key, value):
+        index = hash(key) % 10
+        self.data[index] = [key, value]
+
+    def hash(self, key):
+        hash_value = hash(key) 
+        print(hash_value)
+        return hash_value
+        
+
+
+       
+         
+        
+
+
+
+    
+
+    
+        
+    
+    
+        
+
+
